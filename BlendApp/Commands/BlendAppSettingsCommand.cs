199fa3e7795438d3201ace6ef1862f35488a47d4
Mateusz Szostok <szostok.mateusz@gmail.com>
2015-10-24 16:48:22 +0200

    Add Blend Command

---
+++ BlendApp/Commands/BlendAppSettingsCommand.cs
@@ -0,0 +1,47 @@
+﻿namespace BlendApp.Commands
+{
+    using BlendApp.ViewModels;
+    using System;
+    using System.Windows.Input;
+
+    class BlendAppSettingsCommand : ICommand
+    {
+        #region Members
+        private MainWindowViewModel viewModel;
+        #endregion
+
+        #region Constructors
+        public BlendAppSettingsCommand(MainWindowViewModel view)
+        {
+            viewModel = view;
+        }
+        #endregion
+
+       
+
+        #region ICommand Members
+        public event System.EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return String.IsNullOrWhiteSpace(viewModel.AppSettings.Error);
+        }
+
+
+        public void Execute(object parameter)
+        {
+            viewModel.BlendImages();
+        }
+        #endregion
+    }
+}
